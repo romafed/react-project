34f8a7f835e9daec65d31596db8b28b72584c0a3
Roman <githubkrvchk@gmail.com>
2019-04-23 16:51:41 +0300

    Updates

---
+++ precache-manifest.691409dcb80b8d8911e27bac1bab4cab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586e275bbdb1622f631c4b71abe22589",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "e5531ef31bf899a92dab",
+    "url": "/react-project/static/css/main.c2e4ef54.chunk.css"
+  },
+  {
+    "revision": "5e3d596077edb070d133",
+    "url": "/react-project/static/js/2.24058832.chunk.js"
+  },
+  {
+    "revision": "e5531ef31bf899a92dab",
+    "url": "/react-project/static/js/main.615200aa.chunk.js"
+  },
+  {
+    "revision": "c38e84bee75be2db3df6",
+    "url": "/react-project/static/js/runtime~main.270f79fb.js"
+  }
+]);
